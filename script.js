007f7d4735fe647fb546b883598c8ff2898465c1
Gkamashidze <gqamashidze51@gmail.com>
2023-05-29 23:23:28 +0400

    განახლება 1.0

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-function check() {
-    var ricxvi = document.getElementById("number").value;
+var index = 1;
 
-    if (ricxvi < 0) {
-        document.getElementById("dark").style.display = "flex";
-        document.getElementById("message").innerHTML = "შეყვანილი რიცხვი არის უარყოფითი!"
-    } else if (ricxvi > 0) {
-        document.getElementById("dark").style.display = "flex";
-        document.getElementById("message").innerHTML = "შეყვანილი რიცხვი არის დადებითი!"
-    }
+while (index <= 50) {
+
+    document.write(index + " ");
+    index++;
 }
 
-function closebtn() {
-    document.getElementById("dark").style.display = "none";
-}
+if (index % 2 == 0) {
 
-function show() {
 
-    var ricxvi = document.getElementById("number").value;
-
-    if (ricxvi < 0) {
-        document.getElementById("box1").style.height = "400px"
-        document.getElementById("box1").style.backgroundColor = "red"
-    } else if (ricxvi > 0) {
-        document.getElementById("box1").style.height = "400px"
-        document.getElementById("box1").style.backgroundColor = "green"
-    } else if (ricxvi == 0) {
-        document.getElementById("box1").style.height = "400px"
-        document.getElementById("box1").style.backgroundColor = "grey"
-    }
+} else if (index % 2 == 1) {
 
 }
+
+
