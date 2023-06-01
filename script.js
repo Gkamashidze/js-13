2d5f6830a14630d3b9c4bb59e85d0d3434c09957
Gkamashidze <gqamashidze51@gmail.com>
2023-06-02 00:51:27 +0400

    განახლება 1.0

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
-var ricxvi = 1;
-var ricxvi2 = 1;
+// var ricxvi = 1;
+// var ricxvi2 = 1;
 
-let num = "";
-let numb = "";
+// let num = "";
+// let numb = "";
 
-while (ricxvi <= 50) {
-    if (ricxvi % 2 == 1) {
-        numb += ricxvi + " ";
-        document.getElementById("red").innerHTML = numb;
-        document.getElementById("red").style.color = "red";
+// while (ricxvi <= 50) {
+//     if (ricxvi % 2 == 1) {
+//         numb += ricxvi + " ";
+//         document.getElementById("red").innerHTML = numb;
+//         document.getElementById("red").style.color = "red";
 
+//     }
+//     ricxvi++;
+// }
+
+// while (ricxvi2 <= 50) {
+//     if (ricxvi2 % 2 == 0) {
+//         num += ricxvi2 + " ";
+//         document.getElementById("green").innerHTML = num;
+//         document.getElementById("green").style.color = "green";
+//     }
+//     ricxvi2++;
+// }
+
+var index = 1;
+
+while (index <= 50) {
+    if (index % 2 == 0) {
+        document.write('<font color="green">');
+        document.write(index + " ");
+        index++;
+    } else if (index % 2 == 1) {
+        document.write('<font color="red">');
+        document.write(index + " ");
+        index++;
     }
-    ricxvi++;
-}
-
-while (ricxvi2 <= 50) {
-    if (ricxvi2 % 2 == 0) {
-        num += ricxvi2 + " ";
-        document.getElementById("green").innerHTML = num;
-        document.getElementById("green").style.color = "green";
-    }
-    ricxvi2++;
 }
