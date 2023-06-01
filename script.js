7e240a11767026ff37ee21a4942ba2d21b9908ab
Gkamashidze <gqamashidze51@gmail.com>
2023-06-01 21:32:12 +0400

    განახლება

--- script.js
+++ script.js
@@ -1,36 +1,16 @@
-// var index = 1;
 
-// while (index <= 50) {
+var index = 1;
+// var x = document.getElementById("text").value;
 
-//     document.write(index + " ");
-//     index++;
-// }
+while (index <= 50) {
 
-// if (index % 2 == 0) {
-
-
-// } else if (index % 2 == 1) {
-
-// }
-
-function daiwye() {
-
-    var index = 1;
-    // var x = document.getElementById("text").value;
-
-    while (index <= 50) {
-
-        document.getElementById("text").innerHTML = index;
-
-        // document.write(index + " ");
-
-        index++;
-
-        if (index % 2 == 0) {
-            document.getElementById("text").style.color = "red";
-        }
-    }
+    document.getElementById("text").innerHTML = index;
+    // document.write(index + " ");
+    index++;
 
 
 
+    // if (index % 2 == 0) {
+    //     document.getElementById("text").style.color = "red";
+    // }
 }
